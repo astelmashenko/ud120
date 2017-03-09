77e6d704e31deb4154b198923f0577671242b21f
Andrey Stelmashenko <andrey.stelmashenko@gmail.com>
2017-03-09 23:10:40 +0200

    DT algos

---
+++ gaussnb_train_data/accuracyDT.py
@@ -0,0 +1,35 @@
+import sys
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+import numpy as np
+import pylab as pl
+from sklearn import tree
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+#################################################################################
+
+
+########################## DECISION TREE #################################
+
+
+
+#### your code goes here
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test, pred)
+print acc
+
+#0.908
+
+### be sure to compute the accuracy on the test set
+
+
+
+def submitAccuracies():
+    return {"acc": round(acc, 3)}
+
